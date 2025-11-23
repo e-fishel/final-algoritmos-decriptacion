38289ec90ed0561f7040ac512aa39169b9ccc565
e-fishel <sdlt4545@gmail.com>
2025-11-22 22:11:19 -0600

    boilerplate set up

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vll = vector <ll>;
+using ii = pair <ll, ll>;
+using vii = vector <ii>;
 
+int main () {
+    return 0;
+}
