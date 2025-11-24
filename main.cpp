e4d2c779cd5775cd04d84f3cc3ff566fbbd11202
e-fishel <sdlt4545@gmail.com>
2025-11-24 12:05:28 -0600

    remove extra boilerplate

--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-using vll = vector <ll>;
-using ii = pair <ll, ll>;
-using vii = vector <ii>;
 
 string decode_function (string str) {
-    for (ll i = 0; i < str.size(); i++) {
+    for (int i = 0; i < str.size(); i++) {
         str[i] -= i*i%5;
     }
     return str;
