79367b0d9f7a04d8dc6113ada496cb4c3c4ff544
e-fishel <sdlt4545@gmail.com>
2025-11-27 13:28:44 -0600

    added comments

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string decode_function (string str) {
+string decode_function (string str) { // shallow copy del string
     for (int i = 0; i < str.size(); i++) {
-        str[i] -= i*i%5;
+        str[i] -= i*i%5; // para cada caracter del string, restarle en ascii i^2 mod 5
     }
-    return str;
+    return str; // retornar el string
 }
 
 int main () {
-    string str = "qvi$oot";
+    string str = "qvi$oot"; // string original
     cout << "El string codificado es  : " << str << '\n';
     string decoded = decode_function(str);
     cout << "El string decodificado es: " << decoded << '\n';
